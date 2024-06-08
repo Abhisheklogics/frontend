23f5e8b6d223b5372a6376ff789ba0f930a262c1
Abhisheklogics <abhi.kumar252004@gmail.com>
2024-06-08 12:33:23 +0530

    all changes

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { visualizer } from 'rollup-plugin-visualizer';
 export default defineConfig({
   server:{
   
@@ -14,12 +14,19 @@ export default defineConfig({
       "/arduino/homeData":'http://localhost:10000/',
      
     } ,
-      
+    plugins: [visualizer()],
     build: {
       rollupOptions: {
-        external: ['/src/main.jsx']
-      }
-    }   
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            }
+          },
+        },
+      },
+    },
+    
     },
   plugins: [react()]
   
