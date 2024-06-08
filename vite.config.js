bc3b605944c574533fccaa8b6337705d7869c46c
Abhisheklogics <abhi.kumar252004@gmail.com>
2024-06-08 12:12:30 +0530

    all changes

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   server:{
-      port:import.meta.VITE_APP_PORT,
+  
     proxy:{
       
-      "/arduino/getespData": 'https://adhyans-backend.onrender.com',
-      "/arduino/datasave":'https://adhyans-backend.onrender.com',
-      "/arduino/getDataArduino":'https://adhyans-backend.onrender.com',
-      "/arduino/getDataRaspberry": 'https://adhyans-backend.onrender.com',
-      "/arduino/homeImagesSave":'https://adhyans-backend.onrender.com',
-      "/arduino/homeData":'https://adhyans-backend.onrender.com',
+      "/arduino/getespData":'http://localhost:10000/' ,
+      "/arduino/datasave":'http://localhost:10000/',
+      "/arduino/getDataArduino":'http://localhost:10000/',
+      "/arduino/getDataRaspberry":'http://localhost:10000/', 
+      "/arduino/homeImagesSave":'http://localhost:10000/',
+      "/arduino/homeData":'http://localhost:10000/',
      
     } ,
       
